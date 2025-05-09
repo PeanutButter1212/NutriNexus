3b6034dc71b611fefb34459fa935d13e21465586
PeanutButter1212 <marcusngzh@gmail.com>
2025-05-09 17:10:31 +0800

    Initial project files
    Uploaded all project files with the login page completed

---
+++ SignUpScreen.js
@@ -0,0 +1,13 @@
+import "./global.css"
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View ,TextInput, TouchableOpacity} from 'react-native';
+import { AntDesign } from '@expo/vector-icons'; 
+
+
+export default function SignUpScreen() {
+    return (
+      <Text>Hi</Text>
+    );
+}
+
+  
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./index.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./LoginScreen.{js,jsx,ts,tsx}",
+    "./SignUpScreen.{js,jsx,ts,tsx}",
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
