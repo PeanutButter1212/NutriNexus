150172958048bc85890106123158f99ad902a16b
Chua Kai Jie, Emerson <emersonchua934@outlook.com>
2025-05-18 02:27:46 +0800

    renaming some files to prevent conflict

--- src/app/ProfileScreen.js
+++ src/app/ProfileScreen.js
@@ -3,12 +3,82 @@ import { StatusBar } from "expo-status-bar";
 import {  StyleSheet,  Text,  ScrollView,  View,  TextInput,  TouchableOpacity,} from "react-native";
 import React, { useState, useEffect } from "react";
 import { useAuth } from "../context/AuthContext";
+<<<<<<< Updated upstream
+=======
+import { LinearGradient } from "expo-linear-gradient";
+import CircularProgress from 'react-native-circular-progress-indicator';
+import {Canvas, Group} from '@shopify/react-native-skia';
+import * as d3 from 'd3'; 
+import BarPath from '../components/BarPath'
+import XAxisText from "../components/XAxisText";
+import DropdownComponent from "../components/Dropper";
+>>>>>>> Stashed changes
 
 export default function Profile({ route, navigation }) {    
      const { session, profile, authMethod } = route.params; 
     
      const { logout } = useAuth();
 
+<<<<<<< Updated upstream
+=======
+     const { width } = useWindowDimensions();
+
+     const [selectedDataType, setSelectedDataType] = useState('Steps'); 
+
+
+      const weeklyStepsData = [
+        { day: "MON", value: 3000 },
+        { day: "TUES", value: 5500 },
+        { day: "WED", value: 10000 },
+        { day: "THURS", value: 8000 },
+        { day: "FRI", value: 9000 },
+        { day: "SAT", value: 4000 },
+        { day: "SUN", value: 3500 },
+      ];
+      const weeklyCaloriesData = [
+        { day: "MON", value: 10000 },
+        { day: "TUES", value: 3500 },
+        { day: "WED", value: 1300 },
+        { day: "THURS", value: 8000 },
+        { day: "FRI", value: 4030 },
+        { day: "SAT", value: 3700 },
+        { day: "SUN", value: 8640 },
+      ];
+
+      const [referenceData, setReferenceData] = useState(weeklyStepsData);
+
+
+
+
+    useEffect(() => {
+        if (selectedDataType === 'Steps') {
+          setReferenceData(weeklyStepsData);
+        } else {
+          setReferenceData(weeklyCaloriesData);
+        }
+      }, [selectedDataType]);
+ 
+     const canvasWidth = width;
+     const canvasHeight = 350; 
+
+     const graphWidth = width;
+     const graphMargin = 20; 
+     const graphHeight = canvasHeight - graphMargin; 
+
+     const xRange = [0, graphWidth]; 
+     const xDomain = referenceData.map((dataPoint) => dataPoint.day); 
+
+     const x = d3.scalePoint().domain(xDomain).range(xRange).padding(1); 
+
+     const yRange = [0, graphHeight];
+
+     const yDomain = [0, d3.max(referenceData, ( (yDataPoint) => yDataPoint.value))];
+
+     const y = d3.scaleLinear().domain(yDomain).range(yRange);
+
+     const barWidth = 35;
+
+>>>>>>> Stashed changes
      const handleLogout = () => {
           logout(authMethod, navigation);
       };
---
+++ src/components/Dropper.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { StyleSheet } from 'react-native';
+import { Dropdown } from 'react-native-element-dropdown';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+
+
+const data = [
+    { label: 'Steps', val: 'Steps'},
+    { label: 'Calories', val: 'Calories' },
+  ];
+
+const DropdownComponent = ({ value, onChange }) => {
+
+    console.log("Current Value: "+ value)
+  return (
+    <Dropdown
+      style={styles.dropdown}
+      placeholderStyle={styles.placeholderStyle}
+      selectedTextStyle={styles.selectedTextStyle}
+      inputSearchStyle={styles.inputSearchStyle}
+      iconStyle={styles.iconStyle}
+      data={data}
+      search
+      maxHeight={300}
+      labelField="label"
+      valueField="val"
+      placeholder="Select item"
+      searchPlaceholder="Search..."
+      value={value}
+      onChange={item => {
+        onChange(item.val);
+      }}
+      renderLeftIcon={() => {
+        const selectedItem = data.find(item => item.val === value);
+        return selectedItem?.val === "Steps" ? 
+                                    (<Ionicons name="footsteps-sharp" size={24} color="#ba4a00" />)
+                                     : (<FontAwesome5 name="fire" size={20} color="#FF7F50" />)
+        
+       } }
+    />
+  );
+};
+
+export default DropdownComponent;
+
+const styles = StyleSheet.create({
+  dropdown: {
+    margin: 16,
+    height: 50,
+    borderBottomColor: 'gray',
+    borderBottomWidth: 0.5,
+  },
+  icon: {
+    marginRight: 5,
+  },
+  placeholderStyle: {
+    fontSize: 16,
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
+  },
+});
