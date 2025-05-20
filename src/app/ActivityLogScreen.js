99c3d22a461de42f8a1b79ada05f92d2be04b669
PeanutButter1212 <marcusngzh@gmail.com>
2025-05-20 15:22:29 +0800

    Added Scanner

---
+++ src/app/ActivityLogScreen.js
@@ -0,0 +1,54 @@
+import "../../global.css";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+import { Image } from "react-native";
+import { useState } from "react";
+import { useAuth } from "../context/AuthContext";
+
+export default function ActivityLogScreen({ navigation }) {
+  return (
+    <View className="flex-1 items-center justify-start bg-green-300 pt-28 px-4">
+      {/* Activity Log*/}
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          gap: 4,
+          marginTop: 3,
+        }}
+      >
+        <Text className="text-3xl font-semibold text-black text-center">
+          Activity Log
+        </Text>
+        {/* Folder image*/}
+        <Image
+          source={require("../../assets/Folder.png")}
+          className="w-40 h-40"
+        />
+      </View>
+      <View style={{ flexDirection: "row", gap: 36 }}>
+        <Text className="text-2xl font-semibold mb-4 text-black text-center ">
+          Food
+        </Text>
+        <Text className="text-2xl font-semibold mb-4 text-black text-center">
+          Calories
+        </Text>
+        <Text className="text-2xl font-semibold mb-4 text-black text-center">
+          Date/Time
+        </Text>
+      </View>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Profile")}
+        className="flex-row items-center justify-center w-full bg-green-600 rounded-xl mt-6 py-3"
+      >
+        <Text className="text-white text-base font-medium font-bold">Back</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
