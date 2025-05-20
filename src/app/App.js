3307138a0365872afd48cf31a2db42526d2fc984
PeanutButter1212 <marcusngzh@gmail.com>
2025-05-20 14:52:55 +0800

    Created Basic Activity Log(Frontend)

--- src/app/App.js
+++ src/app/App.js
@@ -4,44 +4,43 @@ import LoginScreen from "./LoginScreen";
 import SignUpScreen from "./SignUpScreen";
 import OTPScreen from "./OTPScreen";
 import DetailScreen from "./DetailScreen";
-import ProfileScreen from "./ProfileScreen"
+import ActivityLogScreen from "./ActivityLogScreen";
+import ProfileScreen from "./ProfileScreen";
 import { AuthProvider } from "../context/AuthContext";
 import React, { useEffect, useState } from "react";
 import { supabase } from "../lib/supabase";
 
 const Stack = createNativeStackNavigator();
 
-
 export default function App() {
-
   const [session, setSession] = useState(null);
   const [profile, setProfile] = useState(null);
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
+      setSession(session);
       if (session?.user?.id) {
-        fetchProfile(session.user.id); 
+        fetchProfile(session.user.id);
       }
-    })
+    });
     supabase.auth.onAuthStateChange((_event, session) => {
       setSession(session);
       if (session?.user?.id) {
         fetchProfile(session.user.id);
       } else {
-        setProfile(null); 
+        setProfile(null);
       }
-    })
-  }, [])
+    });
+  }, []);
 
-  const fetchProfile = async(userId) => {
+  const fetchProfile = async (userId) => {
     try {
-      const {data, error} = await supabase
-      .from("profiles")
-      .select("*")
-      .eq("id", userId)   
-      .limit(1)
-      
+      const { data, error } = await supabase
+        .from("profiles")
+        .select("*")
+        .eq("id", userId)
+        .limit(1);
+
       if (error) {
         throw error;
       }
@@ -49,44 +48,46 @@ export default function App() {
     } catch (err) {
       throw err;
     }
-
-  }
+  };
 
   return (
-
-    <AuthProvider> 
+    <AuthProvider>
       <NavigationContainer>
-      <Stack.Navigator initialRouteName={"Profile"} >
-      <Stack.Screen
-      name="Login"
-      component={LoginScreen}
-      options={{ headerShown: false }}
-      /> 
-      <Stack.Screen
-        name="SignUp"
-        component={SignUpScreen}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="OTP"
-        component={OTPScreen}
-        options={{ headerShown: true }}
-      />
-      <Stack.Screen
-        name="Detail"
-        component={DetailScreen}
-        options={{ headerShown: false }}
-        initialParams={{ session, profile }}
-      />
-      <Stack.Screen
-        name="Profile"
-        component={ProfileScreen}
-        options={{ headerShown: false }}
-        initialParams={{ session, profile }}
-      />
-      </Stack.Navigator>
-    </NavigationContainer>
+        <Stack.Navigator initialRouteName={"Activity Log"}>
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="SignUp"
+            component={SignUpScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="OTP"
+            component={OTPScreen}
+            options={{ headerShown: true }}
+          />
+          <Stack.Screen
+            name="Detail"
+            component={DetailScreen}
+            options={{ headerShown: false }}
+            initialParams={{ session, profile }}
+          />
+          <Stack.Screen
+            name="Profile"
+            component={ProfileScreen}
+            options={{ headerShown: false }}
+            initialParams={{ session, profile }}
+          />
+          <Stack.Screen
+            name="Activity Log"
+            component={ActivityLogScreen}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </AuthProvider>
-    
   );
 }
