777cf2f13fb30f68e4db9606c603788f2ff6a40a
PeanutButter1212 <marcusngzh@gmail.com>
2025-06-10 14:08:39 +0800

    Added marker

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,13 +1,19 @@
 import React, { useState, useEffect, useRef } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Alert } from "react-native";
 import MapView, { Marker, Circle } from "react-native-maps";
 import * as Location from "expo-location";
 import { useDistance } from "../contexts/DistanceTrackingContext";
+import haversineDistance from "haversine-distance";
+import { useNavigation } from "@react-navigation/native";
 
 export default function MapScreen() {
   const { location, distance } = useDistance();
 
   const mapRef = useRef(null);
+
+  const navigation = useNavigation();
+
+  const [loc1InRadius, setLoc1InRadius] = useState(false);
 
   //camera tracking
 
@@ -17,8 +23,8 @@ export default function MapScreen() {
         {
           latitude: location.latitude,
           longitude: location.longitude,
-          latitudeDelta: 0.005, //smaller value more zoomed in lmk if need change
-          longitudeDelta: 0.005,
+          latitudeDelta: 0.003, //smaller value more zoomed in lmk if need change
+          longitudeDelta: 0.003,
         },
         500 //how long it takes to change animation when moving rn its 0.5sec
       );
@@ -32,6 +38,18 @@ export default function MapScreen() {
       </View>
     );
   }
+
+  //check for location 1
+
+  const loc1Coords = { latitude: 1.350624, longitude: 103.749 };
+
+  useEffect(() => {
+    if (!location) return;
+
+    const distanceToLoc1 = haversineDistance(location, loc1Coords);
+
+    setLoc1InRadius(distanceToLoc1 < 50); // 50 meters radius
+  }, [location]);
 
   return (
     //Since cannot use pedometer we use estimate to determine steps(0.75 is average stride)
@@ -53,7 +71,7 @@ export default function MapScreen() {
         initialRegion={{
           latitude: location.latitude,
           longitude: location.longitude,
-          latitudeDelta: 0.0055,
+          latitudeDelta: 0.005,
           longitudeDelta: 0.005,
         }}
       >
@@ -63,16 +81,19 @@ export default function MapScreen() {
           strokeColor="rgba(0,0,255,0.5)"
           fillColor="rgba(0,0,255,0.2)"
         />
+        {/*location 1*/}
+        <Marker
+          coordinate={loc1Coords}
+          pinColor={loc1InRadius ? "green" : "red"}
+          onPress={() => {
+            if (loc1InRadius) {
+              navigation.navigate("HawkerScreen1");
+            } else {
+              Alert.alert("Too far please move closer to interact");
+            }
+          }}
+        />
       </MapView>
     </View>
   );
 }
-
-/*<Marker
-        coordinate={{
-          latitude: location.latitude,
-          longitude: location.longitude,
-        }}
-        title="You are here"
-      />
-      */
