98f5a61b249559b238a933218ae9461244a9eae9
PeanutButter1212 <marcusngzh@gmail.com>
2025-06-28 20:40:01 +0800

    Remove signup

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -6,52 +6,50 @@ import {
   View,
   TextInput,
   TouchableOpacity,
-  Alert
+  Alert,
 } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import { Image } from "react-native";
-import { useState } from "react"
-import { useAuth } from "../contexts/AuthContext"
+import { useState } from "react";
+import { useAuth } from "../contexts/AuthContext";
 
 export default function LoginScreen({ navigation }) {
   const { googleSignIn, signInWithOTP } = useAuth();
   const [email, setEmail] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
-  
+
   const handleLogin = async () => {
     if (!email.trim()) {
       setErrorMessage("Please fill in all fields!");
       return;
     }
     try {
-      setErrorMessage("")
-      console.log(email)
-      const error = await signInWithOTP(email, navigation); 
+      setErrorMessage("");
+      console.log(email);
+      const error = await signInWithOTP(email, navigation);
       if (error) {
-        setErrorMessage(error)
+        setErrorMessage(error);
       }
-    
-
     } catch (err) {
       setErrorMessage(err.message);
     }
-  
   };
-
 
   const handleGoogleLogIn = async () => {
     try {
       const user = await googleSignIn(navigation);
     } catch (error) {
-      setErrorMessage(error.message); 
+      setErrorMessage(error.message);
     }
   };
-
 
   return (
     <View className="flex-1 items-center justify-start bg-white pt-32 px-6">
       {/* Logo */}
-      <Image source={require("../assets/Logo.png")} className="w-40 h-40 mb-4" />
+      <Image
+        source={require("../assets/Logo.png")}
+        className="w-40 h-40 mb-4"
+      />
 
       {/* NutriNuxus */}
       <Text className="text-5xl font-bold text-green-600">NutriNexus</Text>
@@ -62,7 +60,7 @@ export default function LoginScreen({ navigation }) {
       {/* Username Input */}
       <TextInput
         value={email}
-        onChangeText = {(text) => setEmail(text)}
+        onChangeText={(text) => setEmail(text)}
         placeholder="Email"
         autoCapitalize="none"
         className="w-full border border-gray-300 rounded-xl mt-6 px-4 py-3 text-base"
@@ -79,30 +77,36 @@ export default function LoginScreen({ navigation }) {
       */}
 
       {/* Error Message */}
-         {errorMessage ? (
-        <Text className="text-red-500 font-semibold mt-3" >{errorMessage}</Text>
+      {errorMessage ? (
+        <Text className="text-red-500 font-semibold mt-3">{errorMessage}</Text>
       ) : null}
 
       {/* Google Login Button */}
-      <TouchableOpacity 
-      onPress = {handleGoogleLogIn}
-      className="flex-row items-center justify-center w-full bg-red-500 rounded-xl mt-6 py-3">
+      <TouchableOpacity
+        onPress={handleGoogleLogIn}
+        className="flex-row items-center justify-center w-full bg-red-500 rounded-xl mt-6 py-3"
+      >
         <AntDesign name="google" size={20} color="white" className="mr-2" />
         <Text className="text-white text-base font-medium">
           Continue with Google
         </Text>
       </TouchableOpacity>
       {/* Login Button */}
-      <TouchableOpacity onPress = {handleLogin} className="flex-row items-center justify-center w-full bg-green-600 rounded-xl mt-6 py-3">
+      <TouchableOpacity
+        onPress={handleLogin}
+        className="flex-row items-center justify-center w-full bg-green-600 rounded-xl mt-6 py-3"
+      >
         <Text className="text-white text-base font-medium font-bold">
           Login
         </Text>
       </TouchableOpacity>
-      {/* Dont have account? */}
+
+      {/*
+
       <Text className="text-xl mt-2 text-green-600 pt-8">
         Dont have an account?
       </Text>
-      {/* Sign Up button */}
+   
       <TouchableOpacity
         className="flex-row items-center justify-center w-full bg-white-600 rounded-xl mt-6 py-3"
         onPress={() => navigation.navigate("SignUp")}
@@ -111,6 +115,7 @@ export default function LoginScreen({ navigation }) {
           Sign Up
         </Text>
       </TouchableOpacity>
+      */}
     </View>
   );
 }
