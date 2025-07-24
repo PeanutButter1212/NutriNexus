700fd7554d6aa11b09878900ad3ad8aff908e4df
PeanutButter1212 <marcusngzh@gmail.com>
2025-07-24 16:57:14 +0800

    remove garden int test

--- components/InventoryColumn.js
+++ components/InventoryColumn.js
@@ -1,36 +1,38 @@
-import { View, Text, Platform } from 'react-native'
-import React from 'react'
+import { View, Text, Platform } from "react-native";
+import React from "react";
 
-const InventoryColumn = ({topItem, className}) => {
+const InventoryColumn = ({ topItem, className }) => {
   return (
-    <View className={`flex-col ${className || ""}`}> 
+    <View
+      className={`flex-col ${className || ""}`}
+      testID={`inventory-container-${topItem.item_id}`}
+    >
+      <View
+        className={`bg-amber-500 ${
+          Platform.OS === "ios" ? "w-36 h-36" : "w-32 h-32"
+        } justify-center items-center`}
+      >
         <View
-        className={`bg-amber-500 ${Platform.OS === "ios" ? 'w-36 h-36' : 'w-32 h-32'} justify-center items-center`}
+          ref={topItem.slotRef}
+          className={`bg-amber-900 ${
+            Platform.OS === "ios" ? "w-28 h-28" : "w-24 h-24"
+          }  justify-center items-center`}
         >
-            <View
-            ref={topItem.slotRef} 
-            className={`bg-amber-900 ${Platform.OS === "ios" ? "w-28 h-28" : 'w-24 h-24'}  justify-center items-center`}> 
-                <View
-                >
-                    {topItem.children}
-                </View>
-                <View
-                className="bg-emerald-500 absolute right-0 bottom-0 p-1"
-                style={{
-                    borderTopLeftRadius: 10,
-                }}
-                >
-                    <Text 
-                    className="text-white text-center font-bold"
-                    >
-                        {topItem.count}
-                    </Text>
-                </View>
-            </View>
-        </View>  
+          <View>{topItem.children}</View>
+          <View
+            className="bg-emerald-500 absolute right-0 bottom-0 p-1"
+            style={{
+              borderTopLeftRadius: 10,
+            }}
+          >
+            <Text className="text-white text-center font-bold">
+              {topItem.count}
+            </Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
 
-    </View>  
-  )
-}
-
-export default InventoryColumn
+export default InventoryColumn;
